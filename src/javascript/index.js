b2fe0b394e2006065fe5c8599419905f193c2678
MaiconMares <maiconmaresunb@gmail.com>
2021-01-19 22:32:19 -0300

    Updating index and bundling updated files.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,2 +1,9 @@
 import './../style.css';
 console.log('Salut!');
+
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+} else {
+    console.log('Looks like we are in production mode!');
+
+}
